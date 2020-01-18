8db3bb6fb12defcc916e1796028c2ac976208982
Nazar Sukhovych <n.suhovich@gmail.com>
2020-01-18 21:21:23 +0200

    Start work on main screen design.

--- app/src/main/java/com/github/naz013/todoappconcept/MainActivity.kt
+++ app/src/main/java/com/github/naz013/todoappconcept/MainActivity.kt
@@ -3,11 +3,16 @@ package com.github.naz013.todoappconcept
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
+import androidx.databinding.DataBindingUtil
+import com.github.naz013.todoappconcept.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
-
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
     }
 }
